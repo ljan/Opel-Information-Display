01785822501ac7507a37a746d3929d2b0b2569c9
ljan <schick.ne.email+git@gmail.com>
2018-02-12 22:09:40 +0100

    runs on esp8266

--- ReadBus/ReadBus.ino
+++ ReadBus/ReadBus.ino
@@ -15,9 +15,10 @@
 #include <Wire.h>
 
 void setup() {
-  Wire.begin(TID);                // join i2c bus with address #0x94 for TID
+  Wire.begin(TID);              // join i2c bus with address #0x94 for TID
   Wire.onReceive(receiveEvent); // register event
-  Serial.begin(9600);           // start serial for output
+  Serial.begin(74880);          // start serial for output
+  Serial.println("Wire Slave Receiver");
 }
 
 void loop() {
@@ -27,10 +28,10 @@ void loop() {
 // function that executes whenever data is received from master
 // this function is registered as an event, see setup()
 void receiveEvent(int howMany) {
-  while (1 < Wire.available()) { // loop through all but the last
-    char c = Wire.read(); // receive byte as a character
-    Serial.print(c);         // print the character
+  while (1 < Wire.available()) {// loop through all but the last
+    char c = Wire.read();       // receive byte as a character
+    Serial.print(c);            // print the character
   }
-  int x = Wire.read();    // receive byte as an integer
-  Serial.println(x);         // print the integer
+  int x = Wire.read();          // receive byte as an integer
+  Serial.println(x);            // print the integer
 }
